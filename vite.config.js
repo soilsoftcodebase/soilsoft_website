4c7b7452244729e61fda563b0795eb6a09ad854c
anupojudurgesh <anupojudurgesh@gmail.com>
2024-12-11 11:30:26 +0530

    vite changess

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +10,12 @@ export default defineConfig({
     rollupOptions: {
       external: ['react-router-hash-link']
     }
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
 
 
