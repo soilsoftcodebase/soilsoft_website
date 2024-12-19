ce1b937fd3a88e17efdb0d55d312d622eb46fe65
anupojudurgesh <anupojudurgesh@gmail.com>
2024-12-19 16:26:31 +0530

    Final Changes By Durgeshh

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Navbar, Hero, Achievement, Categories, CTA, Footer, RequestDemo, RequestDemo2, Products, Services, MicrosoftForm, CTA2 } from './components';
+import './App.css';
+import { HashLink as Link } from 'react-router-hash-link';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+  
+      <div id="hero">
+        <Hero />
+      </div>
+
+      <div id="services">
+        {/* <RequestDemo2 /> */}
+        <CTA2/>
+        <Services />
+      </div>
+
+      <div id="products">
+        <Products />
+      </div>
+
+      <div id="cta">
+        <CTA />
+      </div>
+
+      <div id="footer">
+        <Footer />    
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,30 @@
+import hamburgerMenu from "./hamburgerMenu.svg";
+import close from "./close.svg";
+import logo from "./logo.svg";
+import lock from "./lock.svg";
+import companyLogo1 from "./company-logo-1.png";
+import companyLogo2 from "./company-logo-2.png";
+import companyLogo3 from "./company-logo-3.png";
+import companyLogo4 from "./company-logo-4.png";
+import blogImg1 from "./blog-1.png";
+import achievement from "./achievement.png";
+import quotationMark from "./quotationMark.png";
+import cta from "./cta.png"
+import cta2 from "./cta2.png"
+
+
+export {
+  hamburgerMenu,
+  close,
+  logo,
+  lock,
+  companyLogo1,
+  companyLogo2,
+  companyLogo3,
+  companyLogo4,
+  blogImg1,
+  achievement,
+  quotationMark,
+  cta,
+  cta2
+};
